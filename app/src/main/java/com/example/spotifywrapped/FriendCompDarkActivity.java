05380199cf3743c087dd61deabcebd7836297ee6
jayanth <jvennamreddy@gmail.com>
2024-03-10 16:51:23 -0400

    Add main activities for core data presentation (base)

---
+++ app/src/main/java/com/example/spotifywrapped/FriendCompDarkActivity.java
@@ -0,0 +1,14 @@
+package com.example.spotifywrapped;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FriendCompDarkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.friendartcompdark);
+    }
+}
