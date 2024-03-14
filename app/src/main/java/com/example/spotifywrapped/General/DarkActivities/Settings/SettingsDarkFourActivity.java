b3b9fe9202dae36e4c66ee6ed80043225bd354de
jayanth <jvennamreddy@gmail.com>
2024-03-14 13:07:15 -0400

    Add removal of files for merge

---
+++ app/src/main/java/com/example/spotifywrapped/General/DarkActivities/Settings/SettingsDarkFourActivity.java
@@ -0,0 +1,16 @@
+package com.example.spotifywrapped.General.DarkActivities.Settings;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spotifywrapped.R;
+
+public class SettingsDarkFourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settingsdark4);
+    }
+}
---
+++ app/src/main/java/com/example/spotifywrapped/General/LightActivities/Settings/SettingsLightFourActivity.java
@@ -0,0 +1,16 @@
+package com.example.spotifywrapped.General.LightActivities.Settings;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spotifywrapped.R;
+
+public class SettingsLightFourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settingslight4);
+    }
+}
