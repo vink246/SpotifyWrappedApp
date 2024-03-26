188ddcbff6f59320d749daed7f0f1d88e1977d64
ebaker65 <ebaker65@gatech.edu>
2024-03-26 01:31:35 -0400

    Adding getters, setters, and toString in SpotifyWrapped.

--- app/src/main/java/com/example/spotifywrapped/spotifyServices/SpotifyWrapped.java
+++ app/src/main/java/com/example/spotifywrapped/spotifyServices/SpotifyWrapped.java
@@ -14,15 +14,15 @@ public class SpotifyWrapped {
 
     public String summaryId;
     public String username;
-    private String timespan;
+    private String timespan; // TODO consider enum here?
     private ArrayList<Track> tracks;
     private ArrayList<Artist> artists;
     private String topGenre;
     private int totalMinutes;
 
     public SpotifyWrapped(String summaryId, String username, String timeSpan) {
-       this.summaryId = summaryId;
-       this.username = username;
+       setSummaryId(summaryId);
+       setUsername(username);
        this.timespan = timespan;
        //TODO populate topTracks, topArtists, topGenre, and totalMinutes based on timespan
 
@@ -35,9 +35,47 @@ public class SpotifyWrapped {
             // add code to refresh the activity here
         });
 
-        topGenre = findTopGenre();
+        this.topGenre = findTopGenre();
 
         //TODO find way to get total minutes listened
+    }
+
+    public Track getTopTrack() {
+        return tracks.get(0);
+    }
+
+    public ArrayList<Track> getTrackList() {
+        return tracks;
+    }
+
+    public Artist getTopArtist() {
+        return artists.get(0);
+    }
+
+    public ArrayList<Artist> getArtistList() {
+        return artists;
+    }
+
+    public String getGenre() {
+        return topGenre;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setSummaryId(String summaryId) {
+        this.summaryId = summaryId;
+    }
+
+    public void setTimespan(String timespan) {
+        this.timespan = timespan;
+        // TODO repopulate variables based on new timespan (where is timespan in constructor?)
+        this.topGenre = findTopGenre();
+    }
+
+    public String getImage() {
+        return artists.get(0).getImage();
     }
 
     private String findTopGenre() {
@@ -65,5 +103,22 @@ public class SpotifyWrapped {
             }
         }
         return genres.get(genreIndex);
+    }
+
+    public String toSting() {
+        return "Top Songs:\n1. " +
+                tracks.get(0).getName() + "\n2. " +
+                tracks.get(1).getName() + "\n3. " +
+                tracks.get(2).getName() + "\n4. " +
+                tracks.get(3).getName() + "\n5. " +
+                tracks.get(4).getName() + "\nTop Artists:\n1. " +
+                artists.get(0).getName() + "\n2. " +
+                artists.get(1).getName() + "\n3. " +
+                artists.get(2).getName() + "\n4. " +
+                artists.get(3).getName() + "\n5. " +
+                artists.get(4).getName() + "\nTop Genre:\n" +
+                topGenre + "\nTotal Minutes Listened:\n" + totalMinutes;
+
+
     }
 }
