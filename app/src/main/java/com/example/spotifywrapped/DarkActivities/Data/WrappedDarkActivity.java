9c76a30bb012e54d7c9125693cdaf82b62867610
Vineet Kulkarni <2vineetk@gmail.com>
2024-03-26 14:38:16 -0400

    Add provider tests

--- app/src/main/java/com/example/spotifywrapped/DarkActivities/Data/WrappedDarkActivity.java
+++ app/src/main/java/com/example/spotifywrapped/DarkActivities/Data/WrappedDarkActivity.java
@@ -1,16 +1,30 @@
 package com.example.spotifywrapped.DarkActivities.Data;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.spotifywrapped.R;
+import com.example.spotifywrapped.spotifyServices.SpotifyProvider;
 
 public class WrappedDarkActivity extends AppCompatActivity {
+
+    SpotifyProvider provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wrappeddark);
+
+        // provider tests - can remove later
+        provider = SpotifyProvider.getInstance();
+
+        provider.getTopTracks(10, 0, SpotifyProvider.WrappedTerm.medium_term, topTracks -> {
+            Log.d("WrappedDarkActivity", topTracks.toString());
+        });
+        provider.getTopArtists(10, 0, SpotifyProvider.WrappedTerm.medium_term, topArtists -> {
+            Log.d("WrappedDarkActivity", topArtists.toString());
+        });
     }
 }
