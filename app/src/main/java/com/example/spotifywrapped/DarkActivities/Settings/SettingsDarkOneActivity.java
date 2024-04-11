c5a0ddcd1f50f10eb0b3262b767c556f929675fb
jayvenn21 <jvennamreddy@gmail.com>
2024-04-11 16:38:29 -0400

    Edit settings UI with position and color

--- app/src/main/java/com/example/spotifywrapped/DarkActivities/Settings/SettingsDarkOneActivity.java
+++ app/src/main/java/com/example/spotifywrapped/DarkActivities/Settings/SettingsDarkOneActivity.java
@@ -31,33 +31,9 @@ public class SettingsDarkOneActivity extends AppCompatActivity {
         setContentView(R.layout.settingsdark1);
 
         // Initialize switches
-        switch1 = findViewById(R.id.switch1);
-        switch2 = findViewById(R.id.switch2);
-        switch3 = findViewById(R.id.switch3);
         switch4 = findViewById(R.id.switch4);
 
         // Set listeners to switches
-        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                switch1.setText(isChecked ? "Light Mode" : "Dark Mode");
-            }
-        });
-
-        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                switch2.setText(isChecked ? "On" : "Off");
-            }
-        });
-
-        switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                switch3.setText(isChecked ? "On" : "Off");
-            }
-        });
-
         switch4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
