fd33cd0063b57b571b05c73148b9504545c7da06
allentmai <amai36@gatech.edu>
2024-04-11 06:40:45 -0400

    - Added javadoc for Comments, User, Wrap java classes. Moved unused methods for FirebaseProvider at the bottom with a comment to show where they are.

--- app/src/main/java/com/example/spotifywrapped/models/User.java
+++ app/src/main/java/com/example/spotifywrapped/models/User.java
@@ -27,7 +27,9 @@ public class User implements Serializable {
     @PropertyName("savedWraps")
     public List<Wrap> savedWraps;
 
-    // Default constructor required for Firebase
+    /**
+     * Default constructor with no parameters purely for Firebase.
+     */
     public User() {
         // Initialize lists to prevent NullPointerException
         this.friends = new ArrayList<>();
@@ -36,7 +38,14 @@ public class User implements Serializable {
         this.savedWraps = new ArrayList<>();
     }
 
-    // Constructor with parameters
+
+    /**
+     * Construtor for Users with the listed parameters below.
+     *
+     * @param username Username of the user.
+     * @param email Email address of the user.
+     * @param isPublic boolean for whether the user's account is public.
+     */
     public User(String username, String email, boolean isPublic) {
         this.username = username;
         this.email = email;
@@ -48,59 +57,114 @@ public class User implements Serializable {
         this.savedWraps = new ArrayList<>();
     }
 
-    // Getters and Setters
+    /**
+     * A getter for profile's username.
+     * @return username Profile's username.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * A setter for  profile's username.
+     * @param username Sets the username to whatever is inputted.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * A getter for user's email.
+     * @return email Email of user.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * A setter for email.
+     * @param email Sets email to whatever is inputted.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * A getter for friend's list.
+     * @return friends List of friends.
+     */
     public List<String> getFriends() {
         return friends;
     }
 
+    /**
+     * A setter for friend's list.
+     * @param friends Sets friendList to whatever is inputted.
+     */
     public void setFriends(List<String> friends) {
         this.friends = friends;
     }
 
+    /**
+     * A getter for outgoingFriendRequests.
+     * @return outgoingFriendRequest Returns list of outgoingFriendRequests.
+     */
     public List<String> getOutgoingFriendRequests() {
         return outgoingFriendRequests;
     }
 
+    /**
+     * A setterf or outgoingFriendRequests.
+     * @param outgoingFriendRequests Sets new list for outgoingFriendRequests.
+     */
     public void setOutgoingFriendRequests(List<String> outgoingFriendRequests) {
         this.outgoingFriendRequests = outgoingFriendRequests;
     }
 
+    /**
+     * A getter for incomingFriendRequests.
+     * @return incomingFriendRequests List of incomingFriendRequests.
+     */
     public List<String> getIncomingFriendRequests() {
         return incomingFriendRequests;
     }
 
+    /**
+     * A setter for incomingFriendRequests.
+     * @param incomingFriendRequests Sets new list for incomingFriendRequests.
+     */
     public void setIncomingFriendRequests(List<String> incomingFriendRequests) {
         this.incomingFriendRequests = incomingFriendRequests;
     }
 
+    /**
+     * A method that returns boolean if profile is public.
+     * @return isPublic Returns whether profile is public.
+     */
     public boolean isPublic() {
         return isPublic;
     }
 
+    /**
+     * Setter to set boolean public.
+     * @param isPublic boolean that
+     */
     public void setPublic(boolean isPublic) {
         this.isPublic = isPublic;
     }
 
+    /**
+     * A getter for savedWraps.
+     * @return savedWraps Returns user's savedWraps.
+     */
     public List<Wrap> getSavedWraps() {
         return savedWraps;
     }
 
+    /**
+     * A setter for savedWraps.
+     * @param savedWraps Sets a new savedWraps.
+     */
     public void setSavedWraps(List<Wrap> savedWraps) {
         this.savedWraps = savedWraps;
     }
